6c61b81033967c99c56873936863775ebced95e5
Iclal Zeynep Ozer <iclalzeynep80@gmail.com>
2024-08-17 09:55:05 +0300

    some controls added

--- main.py
+++ main.py
@@ -6,5 +6,38 @@ drawingBoard.title("Python Turtle")
 
 instanceTurtle = turtle.Turtle()
 
+def turtleForward():
+    instanceTurtle.forward(100)
+
+def rotateRight():
+    #instanceTurtle.right(10)
+    instanceTurtle.setheading(instanceTurtle.heading() - 10)
+
+def rotateLeft():
+    instanceTurtle.left(10)
+
+def turtleReturnHome():
+    instanceTurtle.penup()
+    instanceTurtle.home()
+    instanceTurtle.pendown()
+
+def clearScreen():
+    instanceTurtle.clear()
+    turtleReturnHome()
+
+def turtlePenUp():
+    instanceTurtle.penup()
+
+def turtlePenDown():
+    instanceTurtle.pendown()
+
+drawingBoard.listen()
+drawingBoard.onkey(fun=turtleForward,key="space")
+drawingBoard.onkey(fun=rotateRight, key="Down")
+drawingBoard.onkey(fun=rotateLeft, key="Up")
+drawingBoard.onkey(fun=clearScreen, key="c")
+drawingBoard.onkey(fun=turtleReturnHome, key="o")
+drawingBoard.onkey(fun=turtlePenUp, key="u")
+drawingBoard.onkey(fun=turtlePenDown, key="d")
 
 turtle.mainloop()
